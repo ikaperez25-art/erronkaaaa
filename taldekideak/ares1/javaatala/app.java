0260937c8d25bf9ebdac3e626fc791ca94e012a9
Ares <ikaperez25@lhusurbil.eus>
2025-10-06 08:49:24 +0200

    Java hasi

--- taldekideak/ares1/javaatala/app.java
+++ taldekideak/ares1/javaatala/app.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class app {
     public static void main(String[] args) {
         
+
+
+ArrayList<String> astekoegunak=new ArrayList<String>();
+
+astekoegunak.add("Astelehena");
+astekoegunak.add("Asteartea");
+astekoegunak.add("Asteazkena");
+astekoegunak.add("Osteguna");
+astekoegunak.add("Ostirala");
+astekoegunak.add("Larunbata");
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Aukeratu asteko eguna");
+int astekoeguna= sc.nextInt();
+System.out.println(astekoegunak);
+
+
+
     }
 }
